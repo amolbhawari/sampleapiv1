f831bc2b6fe9327fb3a968f3e265f0fc2ab81b84
amol.bhawari <amol.bhawari@gmail.com>
2019-10-23 18:56:40 +0530

    New Index page added

---
+++ SampleWebApplication/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return "This is Index Page";
+
+        }
+    }
+}
